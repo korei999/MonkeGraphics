9ec2dc3513591426c8e0986da49ce1025ba87730
korei999 <ju7t1xe@gmail.com>
2025-07-26 03:42:32 +0300

    fix win32 SOA generator

--- src/game/Entity.hh
+++ src/game/Entity.hh
@@ -1,6 +1,7 @@
 #pragma once
 
 #include "adt/math.hh"
+#include "adt/SOA.hh"
 
 namespace game
 {
@@ -10,36 +11,18 @@ enum class ENTITY_TYPE : adt::u8
     REGULAR, LIGHT
 };
 
-/* FIXME: problems with SOA generator on win32 */
-struct Entity
-{
-    struct Bind
-    {
-        adt::StringFixed<128>& sfName;
-        adt::math::V4& color;
-        adt::math::V3& pos;
-        adt::math::Qt& rot;
-        adt::math::V3& scale;
-        adt::math::V3& vel;
-        adt::i16& assetI;
-        adt::i16& modelI;
-        ENTITY_TYPE& eType;
-        bool& bNoDraw;
-    };
-
-    /* */
-
-    adt::StringFixed<128> sfName;
-    adt::math::V4 color;
-    adt::math::V3 pos;
-    adt::math::Qt rot;
-    adt::math::V3 scale;
-    adt::math::V3 vel;
-    adt::i16 assetI;
-    adt::i16 modelI;
-    ENTITY_TYPE eType;
-    bool bNoDraw;
-};
+ADT_GEN_SOA_STRUCT(Entity,
+    (adt::StringFixed<128>, sfName),
+    (adt::math::V4, color),
+    (adt::math::V3, pos),
+    (adt::math::Qt, rot),
+    (adt::math::V3, scale),
+    (adt::math::V3, vel),
+    (adt::i16, assetI),
+    (adt::i16, modelI),
+    (ENTITY_TYPE, eType),
+    (bool, bNoDraw)
+);
 
 } /* namespace game */
 
